009ee62873db38fef359ced514016e6e94709d0b
Carloshpjacinto <carlo.henrique37@gmail.com>
2024-09-27 17:47:31 -0300

    Criando component Banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
 import Button from '../components/button/button'
+import Banner from '../components/banner/Banner'
 
 function Home() {
 
@@ -8,6 +9,7 @@ function Home() {
 
         <>
             <Header/>
+            <Banner image="/about.svg" title="About Us"/>
             <Button buttonStyle={"outline"} arrow>clique aqui</Button>
             <Footer/>
         </>
