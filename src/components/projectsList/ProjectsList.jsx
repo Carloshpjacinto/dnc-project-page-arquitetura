4ccb6fc460f0f473c0d46f0e2899a88132d90807
Carloshpjacinto <carlo.henrique37@gmail.com>
2024-10-02 03:30:45 -0300

    Adicionando api do form

--- src/components/projectsList/ProjectsList.jsx
+++ src/components/projectsList/ProjectsList.jsx
@@ -49,18 +49,22 @@ function ProjectsList() {
 
             <div className="project-grid">
                 {
+                    projects ? 
+                        projects.map((project) => (
 
-                    projects.map((project) => (
+                            <div className="project-card" key={project.id}>
+                                <div className="thumbnail tertiary-background" style={{backgroundImage: `url(${project.thumb})`}}></div>
+                                <h3>{project.title}</h3>
+                                <p>{project.subtitle}</p>
+                                <img src={like} />
 
-                        <div className="project-card" key={project.id}>
-                            <div className="thumbnail tertiary-background" style={{backgroundImage: `url(${project.thumb})`}}></div>
-                            <h3>{project.title}</h3>
-                            <p>{project.subtitle}</p>
-                            <img src={like} />
-
-                        </div>
+                            </div>
                     ))
+
+                    :null
+
                 }
+                
             </div>
         </section>
     )
