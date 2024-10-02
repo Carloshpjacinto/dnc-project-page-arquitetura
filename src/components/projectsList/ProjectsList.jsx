e1ecc9592845799f9143997f0fd3f25bae35bcfc
Carloshpjacinto <carlo.henrique37@gmail.com>
2024-10-02 02:25:02 -0300

    Adicionando api de projects

--- src/components/projectsList/ProjectsList.jsx
+++ src/components/projectsList/ProjectsList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 //CSS
 import './ProjectsList.css'
@@ -7,7 +8,33 @@ import './ProjectsList.css'
 import likeFilled from "../../assets/likeFilled.svg"
 import like from "../../assets/like.svg"
 
+//UTILS
+import { getApiData } from '../../services/apiServices'
+
 function ProjectsList() {
+
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+
+            try{
+
+                const projectsResponse = await getApiData('projects')
+
+                setProjects(projectsResponse)
+
+            }catch{
+
+                setProjects([])
+
+            }
+        }
+
+        fetchData()
+
+    }, [])
 
     return(
 
@@ -21,109 +48,19 @@ function ProjectsList() {
             </div>
 
             <div className="project-grid">
-                <div className="project-card d-flex jc-center al-center fd-column">
-                    <div className="thumbnail tertiary-background">
+                {
 
-                    </div>
+                    projects.map((project) => (
 
-                    <h3>Nome do projeto</h3>
+                        <div className="project-card" key={project.id}>
+                            <div className="thumbnail tertiary-background" style={{backgroundImage: `url(${project.thumb})`}}></div>
+                            <h3>{project.title}</h3>
+                            <p>{project.subtitle}</p>
+                            <img src={like} />
 
-                    <p>Localização</p>
-
-                    <img src={like} alt="Like" className="like-icon"/>
-
-                </div>
-
-                <div className="project-card d-flex jc-center al-center fd-column">
-                    <div className="thumbnail tertiary-background">
-                        
-                    </div>
-
-                    <h3>Nome do projeto</h3>
-
-                    <p>Localização</p>
-
-                    <img src={likeFilled} alt="Like" className="like-icon"/>
-
-                </div>
-
-                <div className="project-card d-flex jc-center al-center fd-column">
-                    <div className="thumbnail tertiary-background">
-                        
-                    </div>
-
-                    <h3>Nome do projeto</h3>
-
-                    <p>Localização</p>
-
-                    <img src={like} alt="Like" className="like-icon"/>
-
-                </div>
-
-                <div className="project-card d-flex jc-center al-center fd-column">
-                    <div className="thumbnail tertiary-background">
-                        
-                    </div>
-
-                    <h3>Nome do projeto</h3>
-
-                    <p>Localização</p>
-
-                    <img src={likeFilled} alt="Like" className="like-icon"/>
-
-                </div>
-
-                <div className="project-card d-flex jc-center al-center fd-column">
-                    <div className="thumbnail tertiary-background">
-                        
-                    </div>
-
-                    <h3>Nome do projeto</h3>
-
-                    <p>Localização</p>
-
-                    <img src={like} alt="Like" className="like-icon"/>
-
-                </div>
-
-                <div className="project-card d-flex jc-center al-center fd-column">
-                    <div className="thumbnail tertiary-background">
-                        
-                    </div>
-
-                    <h3>Nome do projeto</h3>
-
-                    <p>Localização</p>
-
-                    <img src={likeFilled} alt="Like" className="like-icon"/>
-
-                </div>
-
-                <div className="project-card d-flex jc-center al-center fd-column">
-                    <div className="thumbnail tertiary-background">
-                        
-                    </div>
-
-                    <h3>Nome do projeto</h3>
-
-                    <p>Localização</p>
-
-                    <img src={like} alt="Like" className="like-icon"/>
-
-                </div>
-
-                <div className="project-card d-flex jc-center al-center fd-column">
-                    <div className="thumbnail tertiary-background">
-                        
-                    </div>
-
-                    <h3>Nome do projeto</h3>
-
-                    <p>Localização</p>
-
-                    <img src={likeFilled} alt="Like" className="like-icon"/>
-
-                </div>
+                        </div>
+                    ))
+                }
             </div>
         </section>
     )
